54112609705caaa16415471ec2bfa0090a8c3ea7
Bart Reardon <bart.reardon@csiro.au>
2023-03-23 22:07:52 +1100

    Moved location of the installPackage function into FileUtils.swift where it probably belongs

--- Outset/Functions/FileUtils.swift
+++ Outset/Functions/FileUtils.swift
@@ -51,6 +51,42 @@ func runShellCommand(_ command: String, args: [String] = [], verbose: Bool = fal
     return (output, error, status)
 }
 
+func installPackage(pkg: String) -> Bool {
+    // Installs pkg onto boot drive
+    if isRoot() {
+        var pkgToInstall: String = ""
+        var dmgMount: String = ""
+
+        if pkg.lowercased().hasSuffix("dmg") {
+            dmgMount = mountDmg(dmg: pkg)
+            for files in folderContents(path: dmgMount) where ["pkg", "mpkg"].contains(files.lowercased().suffix(3)) {
+                pkgToInstall = dmgMount
+            }
+        } else if ["pkg", "mpkg"].contains(pkg.lowercased().suffix(3)) {
+            pkgToInstall = pkg
+        }
+        writeLog("Installing \(pkgToInstall)")
+        let cmd = "/usr/sbin/installer -pkg \(pkgToInstall) -target /"
+        let (output, error, status) = runShellCommand(cmd, verbose: true)
+        if status != 0 {
+            writeLog(error, logLevel: .error)
+        } else {
+            writeLog(output)
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)) {
+            if !dmgMount.isEmpty {
+                writeLog(detachDmg(dmgMount: dmgMount))
+            }
+        }
+        return true
+    } else {
+        writeLog("Unable to process \(pkg)", logLevel: .error)
+        writeLog("Must be root to install packages", logLevel: .error)
+    }
+    return false
+}
+
 func ensureWorkingFolders() {
     // Ensures working folders are all present and creates them if necessary
     let workingDirectories = [
